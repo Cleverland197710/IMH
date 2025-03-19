ef87af34b86968cbb8ba0c6d3667c5a410dbd4ca
Cleverland197710 <realzacharyjayden@gmail.com>
2025-03-19 11:35:20 +0000

    Push 5

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 movement;
 
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,10 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
