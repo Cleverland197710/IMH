f94809c0f32b8b88c34e3b6684f2428461a87d20
Cleverland197710 <realzacharyjayden@gmail.com>
2025-03-21 15:35:02 +0000

    push 10

--- Assets/Scripts/Throwing.cs
+++ Assets/Scripts/Throwing.cs
@@ -7,13 +7,19 @@ public class Throwing : MonoBehaviour
 
     public Transform AimPoint;
     public GameObject ballPrefab;
+    public static int ball;
 
     public float bulletForce = 20f;
+
+    void Start()
+    {
+        ball = 1;        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && ball >= 1)
         {
             Snipe();
         }
@@ -24,5 +30,6 @@ public class Throwing : MonoBehaviour
         GameObject bullet = Instantiate(ballPrefab, AimPoint.position, AimPoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(AimPoint.up * bulletForce, ForceMode2D.Impulse);
+        ball -= 1;
     }
 }
