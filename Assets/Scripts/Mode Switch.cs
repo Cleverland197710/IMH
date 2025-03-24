e3b5f005abf21c46bfa5877f7861cd6d92c07e1f
Cleverland197710 <realzacharyjayden@gmail.com>
2025-03-24 10:44:50 +0000

    push 11

--- Assets/Scripts/Mode Switch.cs
+++ Assets/Scripts/Mode Switch.cs
@@ -5,42 +5,52 @@ using UnityEngine;
 public class ModeSwitch : MonoBehaviour
 {
 
+    public Rigidbody2D rb;
+
     public bool ThrowMode;
     public bool WalkMode;
 
-    public GameObject Walk;
-    public GameObject Throw;
+    private bool isInWalkMode = false;
+    private bool isInThrowMode = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        // Initial setup based on starting mode
+        isInWalkMode = true;
+        isInThrowMode = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftShift))
         {
+            if (isInWalkMode)
+            {
+                // Switch to Throw mode
+                Debug.Log("Pew!"); // You can customize this message
+                GetComponent<PlayerMovement>().enabled = false;
+                GetComponent<PlayerMShooting>().enabled = true;
+                GetComponent<Throwing>().enabled = true;
+                GetComponent<Animator>().enabled = false;
 
-              if (GetComponent<PlayerMovement>().enabled == false && GetComponent<PlayerMShooting>().enabled == true)
-              {
-                  Debug.Log("WALK!");
-                  GetComponent<PlayerMovement>().enabled = true;
-                  //GetComponent<Animator>().enabled = false;
-                  GetComponent<PlayerMShooting>().enabled = false;
-                  GetComponent<Throwing>() .enabled = false;
-              }
+                isInWalkMode = false;
+                isInThrowMode = true;
+            }
+            else if (isInThrowMode)
+            {
+                // Switch to Walk mode
+                Debug.Log("WALK!"); // Customize this message
+                GetComponent<PlayerMovement>().enabled = true;
+                GetComponent<PlayerMShooting>().enabled = false;
+                GetComponent<Throwing>().enabled = false;
+                GetComponent<Animator>().enabled = true;
+                rb.rotation = 0;
 
-              if (GetComponent<PlayerMovement>().enabled == true && GetComponent<PlayerMShooting>().enabled == false)
-              {
-                  Debug.Log("Pew!");
-                  GetComponent<PlayerMovement>().enabled = false;
-                  //GetComponent <Animator>().enabled = false;
-                  GetComponent<PlayerMShooting>().enabled = true;
-                  GetComponent <Throwing>() .enabled = true;
-              }
-            
+                isInWalkMode = true;
+                isInThrowMode = false;
+            }
         }
     }
 }
