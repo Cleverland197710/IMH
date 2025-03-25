d1f7a43014ee0a64b2a5af4253aec2a4646fb5a5
Cleverland197710 <realzacharyjayden@gmail.com>
2025-03-25 14:45:46 +0000

    push 15

--- Assets/Scripts/Ball Collect.cs
+++ Assets/Scripts/Ball Collect.cs
@@ -1,57 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class BallCollect : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public BoxCollider2D StressBall;
-    public bool colliding;
-    public bool pickUp;
-    public static bool vault;
+    public int value;
 
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        BoxCollider StressBall = GetComponent<BoxCollider>();
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        colliding = true;
-        Invoke("Colliding", 0f);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        colliding = true;
-        Invoke("Colliding", 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (colliding == true)
-        {
-            pickUp = true;
-        }
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            Invoke("Falseify", 1f);
-        }
     }
 
-    void Colliding()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        colliding = true;
-    }
+        /*if (collision.gameObject.CompareTag("Player")) ;
+        {
+            Destroy(gameObject);
+            CoinCounter.instance.IncreaseCoins(value);
+        }*/
 
-    void Falseify()
-    {
-        pickUp = false;
-        colliding = false;
     }
 }
