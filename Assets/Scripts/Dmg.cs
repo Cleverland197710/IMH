e3f55a5d50b5f67d26498aedc019f1270d270b4d
Cleverland197710 <realzacharyjayden@gmail.com>
2025-03-27 10:41:23 +0000

    push 18

--- Assets/Scripts/Dmg.cs
+++ Assets/Scripts/Dmg.cs
@@ -19,12 +19,12 @@ public class Dmg : MonoBehaviour
 
     }
 
-    void OnCollisionEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            if(PlayerSanity == null)
-            { 
+            if (PlayerSanity == null)
+            {
                 PlayerSanity = collision.gameObject.GetComponent<PlayerSanity>();
             }
             PlayerSanity.TakeDamage(Damage);
